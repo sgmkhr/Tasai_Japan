80b7915af7d52be5372faa0339c1ba0a95fccfab
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-05-31 03:34:11 +0000

    [Fix]モデル名修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-devise_for :customers, skip: [:passwords], controllers: {
+devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
