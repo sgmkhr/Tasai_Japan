4a98493e8cbb648c4cc314afbfc42b4113469694
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-01 03:16:18 +0000

    [Add]usersコントローラアクション作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,32 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_selected_user, except: [:index]
   
   def show
   end
 
   def index
+    @users = User.all #退会済みも含む全ユーザーを表示
   end
   
   def cancel
+    @user.update(is_active: true)
+    #このあと非同期処理 jsファイルが呼び出される
   end
   
   def withdraw
+    @user.update(is_active: false)
+    #このあと非同期処理 jsファイルが呼び出される
   end
   
   def destroy
+    @user.destroy
+    redirect_to admin_users_path, notice: I18n.t('admin.users.destroy.notice')
   end
   
+  private
+  
+  def set_selected_user
+    @user = User.find_by_canonical_name_or_id(params[:id])
+  end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,21 +1,44 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_current_user, only: [:edit, :update, :withdraw]
   
   def show
+    @user = User.find_by_canonical_name_or_id(params[:id])
   end
 
   def index
+    @users = User.where(is_active: true).all #退会済みユーザーは表示しない
   end
 
   def edit
   end
   
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: I18n.t('users.update.notice')
+    else
+      flash.now[:alert] = I18m.t('users.update.alert')
+      render :edit
+    end
   end
   
   def withdraw
+    @user.update(is_active: false)
+    reset_session
+    redirect_to new_user_registration_path, notice: I18n.t('users.withdraw.notice')
   end
 
   def unsubscribe
+  end
+  
+  private
+  
+  def set_current_user
+    @user = current_user
+  end
+    
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :public_name, :position, :introduction)
   end
   
 end
