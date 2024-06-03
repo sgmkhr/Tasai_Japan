bafccb15af22c766438b2da91d5cb3047e960da2
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-03 10:18:25 +0000

    [Add]投稿関連画面レイアウト作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,11 +1,17 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @posts = Post.page(params[:page]).per(10)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
   
   def destroy
+    Post.find(params[:id]).destroy
+    redirect_to admin_posts_path, notice: I18n.t('posts.destroy')
   end
   
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,22 +1,52 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_selected_post, except: [:new, :index]
+  
   def new
+    @post = Post.new
   end
 
   def index
+    @posts = Post.page(params[:page]).per(10)
   end
 
   def show
   end
   
   def create
+    if @post.save(post_params)
+      redirect_to post_path(@post.id), notice: I18n.t('posts.create.notice')
+    else
+      flash.now[:alert] = I18n.t('posts.create.alert')
+      render :new
+    end
   end
 
   def edit
   end
   
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: I18n.t('posts.update.notice')
+    else
+      flash.now[:alert] = I18n.t('posts.update.alert')
+      render :edit
+    end
   end
   
   def destroy
+    @post.destroy
+    redirect_to posts_path, notice: I18n.t('posts.destroy')
   end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :caption, :body, :prefecture, :post_image)
+  end
+  
+  def set_selected_post
+    @user = User.find(params[:id])
+  end
+  
 end
