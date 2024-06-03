9758326359a15566855a75eca5748bb77f56a8f9
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-03 11:51:17 +0000

    [Update]メニュー画面のリンク先

--- db/seeds.rb
+++ db/seeds.rb
@@ -248,21 +248,21 @@ User.find_by(canonical_name: 'aoi16').posts.create!(
       caption: '福岡のドライブスポットといえば！',
       body: 'この前はドライブではなく、遊覧船に乗って鍾乳洞を覗きにいけるアクティビティに参加してきました。',
       prefecture: 7,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_itoshima.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_itoshima.jpeg")),filename: 'fukuoka_itoshima.jpeg')
     },
     {
       title: '明太子のお重',
       caption: '博多に来たら必ず行ってほしい',
       body: '明太子好きにはたまらない！重厚な雰囲気だけどカジュアルにランチで楽しめる。',
       prefecture: 7,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_mentaizyu.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_mentaizyu.jpeg")),filename: 'fukuoka_mentaizyu.jpeg')
     },
     {
       title: '柳川',
       caption: 'うなぎも有名な場所',
       body: '福岡市からは少し離れていますが、美味しい鰻重と穏やかな川下りが楽しめますよ。',
       prefecture: 7,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_yanagawa.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/fukuoka_yanagawa.jpeg")),filename: 'fukuoka_yanagawa.jpeg')
     }
   ]
 )
@@ -275,28 +275,28 @@ User.find_by(canonical_name: 'dan17').posts.create!(
       caption: '関東のスキー場といえばここ！',
       body: '関東に住まいの方に人気のスキー場。上の方に登ると、素敵な景色と共にビールも飲めちゃいます！',
       prefecture: 20,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/nagano_ryuo.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/nagano_ryuo.jpeg")),filename: 'nagano_ryuo.jpeg')
     },
     {
       title: '秩父',
       caption: '東京から日帰りドライブにおすすめ',
       body: '秩父では荒川のライン下りが楽しいですよ！ランチはここがお気に入り。新鮮なジビエを岩の上で焼いて、きのこもたっぷり、自然の中で味わえる贅沢ランチです！',
       prefecture: 11,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/saitama_chichibu.jpg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/saitama_chichibu.jpg")),filename: 'saitama_chichibu.jpg')
     },
     {
       title: '浅草寺',
       caption: '日本の象徴的な観光スポット',
       body: '浅草寺の雷門が有名ですが、奥に進んだ場所から見えるこの眺めもおすすめです。夜は空いているので写真も撮りやすいですよ。',
       prefecture: 13,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_asakusa.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_asakusa.jpeg")),filename: 'tokyo_asakusa.jpeg')
     },
     {
       title: '渋谷スカイ',
       caption: '渋谷にできた高層商業施設の屋上！',
       body: '入場料は少しお高めですが、ここからの展望は本当に最高！芝生があるのでゆっくりできますよ。',
       prefecture: 13,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_shibuya.jpg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_shibuya.jpg")),filename: 'tokyo_shibuya.jpg')
     }
   ]
 )
@@ -309,14 +309,14 @@ User.find_by(canonical_name: 'ritsu18').posts.create!(
       caption: '付知川の清流',
       body: '本当に今まで見た川の中で一番エメラルド色！心も癒されるので、自然が好きな人にはぜひ訪れていただきたい！',
       prefecture: 21,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/gifu_river.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/gifu_river.jpeg")),filename: 'gifu_river.jpeg')
     },
     {
       title: '白川郷',
       caption: 'ここでしか見れない景色',
       body: '観光地として日本人にとっては馴染み深いですが、海外から来た人はあまり知らないかも。感動すること間違いなし！',
       prefecture: 21,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/gifu_shirakawago.jpg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/gifu_shirakawago.jpg")),filename: 'gifu_shirakawago.jpg')
     }
   ]
 )
@@ -329,28 +329,28 @@ User.find_by(canonical_name: 'yuma19').posts.create!(
       caption: '県名よりもよく知られている場所',
       body: '程よく品もあるエリア。遊ぶスポットもたくさんある、栄えた場所です。住む場所としても人気があります！',
       prefecture: 28,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/hyogo_kobe.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/hyogo_kobe.jpeg")),filename: 'hyogo_kobe.jpeg')
     },
     {
       title: '江ノ電',
       caption: 'ドライブも良し、電車旅も良し',
       body: '関東エリアに住まいの人々は定期的に足を運ぶような場所。気軽に海を眺めに行けます。',
       prefecture: 14,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kanagawa_enoden.jpg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kanagawa_enoden.jpg")),filename: 'kanagawa_enoden.jpg')
     },
     {
       title: '鴨川',
       caption: '京都の代表的な景色',
       body: '5月から夏にかけては川沿いに川床が出されることでも有名。ランクの高い飲食店も多く、鴨川を眺めながら贅沢な食事の時間を過ごせます。',
       prefecture: 26,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kyoto_kamogawa.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kyoto_kamogawa.jpeg")),filename: 'kyoto_kamogawa.jpeg')
     },
     {
       title: '京都で日本酒',
       caption: 'お酒好きにはたまりません！',
       body: '日本酒の飲み比べに加えて、京都では、おちょこも選べちゃうお店が多い！',
       prefecture: 26,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kyoto_nihonsyu.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/kyoto_nihonsyu.jpeg")),filename: 'kyoto_nihonsyu.jpeg')
     }
   ]
 )
@@ -363,14 +363,14 @@ User.find_by(canonical_name: 'yuka20').posts.create!(
       caption: '名前は東京だけど千葉にあるよ！',
       body: '年中平和な雰囲気が漂っているテーマパーク！でも、冬のイルミネーションの時期は一味違います！！規模が桁違いの、すばらしい景色が観れるので、ぜひこの冬訪れてみてください！',
       prefecture: 12,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/chiba_doitumura.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/chiba_doitumura.jpeg")),filename: 'chiba_doitumura.jpeg')
     },
     {
       title: 'いすみ市',
       caption: '星を見るならここ',
       body: '田舎町ではありますが、グランピング施設が多く点在しており、喧騒から離れて自然の中で癒されたい時におすすめのスポットです。',
       prefecture: 12,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/chiba_isumi.jpg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/chiba_isumi.jpg")),filename: 'chiba_isumi.jpg')
     }
   ]
 )
@@ -383,14 +383,14 @@ User.find_by(canonical_name: 'henry4').posts.create!(
       caption: 'A beautiful tower',
       body: 'The color change on each day. You can see the tower in mostly red but sometimes other colors.',
       prefecture: 13,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_tower.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/tokyo_tower.jpeg")),filename: 'tokyo_tower.jpeg')
     },
     {
       title: 'Kamosui',
       caption: 'Aquarium',
       body: 'I visited Yamagata and found this beautiful aquarium. It was amazing.',
       prefecture: 6,
-      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/yamagata_kamosui.jpeg")),filename: 'no_image.jpg')
+      post_image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/yamagata_kamosui.jpeg")),filename: 'yamagata_kamosui.jpeg')
     }
   ]
 )
