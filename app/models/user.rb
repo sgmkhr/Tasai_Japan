bef55846f77261a5a3837323ff37322bc9bd7684
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-03 03:04:19 +0000

    [Add]投稿コントローラ作成、ルーティング設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   
-  has_many: posts, dependent: :destroy
+  has_many :posts, dependent: :destroy
 
   enum position: { beginner: 0, intermediate: 1, veteran: 2 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,14 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :index, :destroy], param: :canonical_name
     patch 'users/:canonical_name/cancel', to: 'users#cancel', as: 'cancel'
     patch 'users/:canonical_name/withdraw', to: 'users#withdraw', as: 'withdraw'
+    resources :posts, only: [:index, :show, :destroy]
   end
 
   devise_for :users, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions:      'public/sessions'
   }
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
@@ -27,6 +28,7 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :index, :edit, :update], param: :canonical_name
     patch 'users/:canonical_name/withdraw', to: 'users#withdraw', as: 'withdraw'
     get 'users/:canonical_name/unsubscribe', to: 'users#unsubscribe', as: 'unsubscribe'
+    resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
