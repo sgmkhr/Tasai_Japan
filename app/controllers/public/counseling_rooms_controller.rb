6bdb87a73910198327a5deba79b3082d73f7f7b5
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-06 09:04:12 +0000

    [Add]カテゴリ、相談室、参加のアクション作成

--- app/controllers/public/counseling_rooms_controller.rb
+++ app/controllers/public/counseling_rooms_controller.rb
@@ -1,13 +1,75 @@
 class Public::CounselingRoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category
+  before_action :ensure_room_creator, only: [:edit, :update, :destroy]
+  
   def index
+    @counseling_rooms = @category.counseling_rooms.page(params[:page]).per(20)
   end
 
   def new
+    @counseling_room = CounselingRoom.new
+  end
+  
+  def create
+    @counseling_room = CounselingRoom.new(counseling_room_params)
+    @counseling_room.category_id = @category.id
+    if @counseling_room.save
+      redirect_to category_counseling_room_path(@category.id, @counseling_room.id), notice: I18n.t('counseling_rooms.create.succeeded')
+    else
+      flash.now[:alert] = I18n.t('counseling_rooms.create.failed')
+      render :new
+    end
   end
 
   def show
+    @counseling_room = CounselingRoom.find(params[:id])
+    @participations = @counseling_room.participations.where(status: true) #参加承認を受けているユーザーのみ表示
   end
 
   def edit
+    @counseling_room = CounselingRoom.find(params[:id])
   end
+  
+  def update
+    @counseling_room = CounselingRoom.find(params[:id])
+    if @counseling_room.update(counseling_room_params)
+      redirect_to category_counseling_room_path(@category.id, @counseling_room.id), notice: I18n.t('counseling_rooms.update.succeeded')
+    else
+      flash.now[:alert] = I18n.t('counseling_rooms.update.failed')
+      render :edit
+    end
+  end
+  
+  def destroy
+    CounselingRoom.find(params[:id]).destroy
+    redirect_to category_counseling_rooms_path(@category.id), notice: I18n.t('counseling_rooms.destroy')
+  end
+  
+  private
+  
+  def counseling_room_params
+    params.require(:counseling_room).permit(:topic, :detail)
+  end
+  
+  # def ensure_participated_user
+  #   counseling_room = CounselingRoom.find(params[:id])
+  #   return if  current_user == counseling_room.user #相談室作成者は当アクション処理を抜ける
+  #   participation = counseling_room.participations.where(user_id: current_user.id)
+  #   unless participation.status #参加承認済みのユーザー
+  #     redirect_to category_counseling_rooms_path(params[:category_id]), alert: I18n.t('counseling_rooms.validates.user')
+  #   end
+  # end
+  
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+  
+  def ensure_room_creator
+    counseling_room = CounselingRoom.find(params[:id])
+    unless current_user == counseling_room.user
+      redirect_to category_counseling_room_path(@category.id, counseling_room.id), notice: I18n.t('counseling_rooms.ensure_creator')
+    end
+  end
+  
 end
--- app/controllers/public/participations_controller.rb
+++ app/controllers/public/participations_controller.rb
@@ -1,2 +1,41 @@
 class Public::ParticipationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category_and_room, only: [:create, :destroy]
+  before_action :ensure_room_creator,   only: [:update]
+  
+  def create
+    @counseling_room.participations.create(user_id: current_user.id)
+    # 非同期通信のため、participations/create.js.erbが呼び出される
+  end
+  
+  def update
+    participation = Participation.find(params[:id])
+    participation.update(status: true)
+    redirect_to request.referer, notice: I18n.t('participations.update')
+  end
+  
+  def destroy
+    participation = Participation.find(params[:id])
+    if participation.user == current_user   #参加者による脱退の場合
+      participation.destroy
+      redirect_to category_counseling_room_path(@category.id, @counseling_room.id), notice: I18n.t('participations.destroy.by_participation')
+    else                                    #作成者による参加申請却下の場合
+      participation.destroy
+      redirect_to request.referer, notice: I18n.t('participations.destroy.by_creator')
+    end
+  end
+  
+  private
+  
+  def set_category_and_room
+    @category = Category.find(params[:category_id])
+    @counseling_room = CounselingRoom.find(params[:counseling_room_id])
+  end
+  
+  def ensure_room_creator
+    unless current_user == Participation.find(params[:id]).counseling_room.user
+      redirect_to request.referer, alert: I18n.t('counseling_rooms.ensure_creator')
+    end
+  end
+  
 end
