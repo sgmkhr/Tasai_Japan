3155b8013ab9dd572c31d9c57683f09a5b4514cd
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-06 04:04:34 +0000

    [Add]コメント削除権限

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,7 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :ensure_comment_author_and_set_comment, only: [:destroy]
+
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
@@ -8,18 +9,24 @@ class Public::CommentsController < ApplicationController
     @comment.save
     # 非同期通信のためcomments/create.jsを呼び出す
   end
-  
+
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
-    @post = Post.find(params[:post_id])
     # 非同期通信のためcomments/destroy.jsを呼び出す
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:content)
   end
-  
+
+  def ensure_comment_author_and_set_comment
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    unless @comment.user == current_user
+      redirect_to post_path(@post.id), alert: I18n.t('comments.validates')
+    end
+  end
+
 end
