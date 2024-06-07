d82687ecd70db7e0d0f7394fbc40244f21eb2ccb
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-07 14:07:44 +0000

    [Add]意見一覧、意見投稿レイアウト

--- app/controllers/public/opinions_controller.rb
+++ app/controllers/public/opinions_controller.rb
@@ -1,9 +1,10 @@
 class Public::OpinionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_category_and_room
   
   def create
     @opinion = current_user.opinions.new(opinion_params)
-    @opinion.counseling_room_id = CounselingRoom.find(params)
+    @opinion.counseling_room_id = @counseling_room.id
     @opinion.save
     @opinions = Opinion.all
     # 非同期通信のため、opinions/create.js.erbを呼び出す
@@ -19,6 +20,11 @@ class Public::OpinionsController < ApplicationController
   
   def opinion_params
     params.require(:opinion).permit(:content)
+  end
+  
+  def set_category_and_room
+    @category = Category.find(params[:category_id])
+    @counseling_room = CounselingRoom.find(params[:counseling_room_id])
   end
   
 end
