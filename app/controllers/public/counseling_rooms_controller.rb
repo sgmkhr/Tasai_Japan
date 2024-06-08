5fb4ba9184175b4fd598c3b161629d68d01b28a8
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-08 09:37:08 +0000

    [Add]カテゴリ、相談室一覧のソート切り替え機能

--- app/controllers/public/counseling_rooms_controller.rb
+++ app/controllers/public/counseling_rooms_controller.rb
@@ -2,9 +2,23 @@ class Public::CounselingRoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_category
   before_action :ensure_room_creator, only: [:edit, :update, :destroy]
-  
+
   def index
-    if params[:content]
+    if params[:latest]
+      @counseling_rooms = @category.counseling_rooms.latest.page(params[:page]).per(20)
+    elsif params[:old]
+      @counseling_rooms = @category.counseling_rooms.old.page(params[:page]).per(20)
+    elsif params[:participations_count]
+      counseling_rooms = @category.counseling_rooms.sort {|a,b|
+        b.participations.where(status: true).size <=> a.participations.where(status: true).size
+      }
+      @counseling_rooms = Kaminari.paginate_array(counseling_rooms).page(params[:page]).per(20)
+    elsif params[:opinions_count]
+      counseling_rooms = @category.counseling_rooms.sort {|a,b|
+        b.opinions.size <=> a.opinions.size
+      }
+      @counseling_rooms = Kaminari.paginate_array(counseling_rooms).page(params[:page]).per(20)
+    elsif params[:content]
       @counseling_rooms = CounselingRoom.search_with_category_for(params[:content], @category).page(params[:page]).per(20)
     else
       @counseling_rooms = @category.counseling_rooms.page(params[:page]).per(20)
@@ -14,7 +28,7 @@ class Public::CounselingRoomsController < ApplicationController
   def new
     @counseling_room = CounselingRoom.new
   end
-  
+
   def create
     @counseling_room = CounselingRoom.new(counseling_room_params)
     @counseling_room.category_id = @category.id
@@ -39,7 +53,7 @@ class Public::CounselingRoomsController < ApplicationController
     @counseling_room = CounselingRoom.find(params[:id])
     @participations = @counseling_room.participations.page(params[:page]).per(10)
   end
-  
+
   def update
     @counseling_room = CounselingRoom.find(params[:id])
     if @counseling_room.update(counseling_room_params)
@@ -49,18 +63,18 @@ class Public::CounselingRoomsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     CounselingRoom.find(params[:id]).destroy
     redirect_to category_counseling_rooms_path(@category.id), notice: I18n.t('counseling_rooms.destroy')
   end
-  
+
   private
-  
+
   def counseling_room_params
     params.require(:counseling_room).permit(:topic, :detail)
   end
-  
+
   # def ensure_participated_user
   #   counseling_room = CounselingRoom.find(params[:id])
   #   return if  current_user == counseling_room.user #相談室作成者は当アクション処理を抜ける
@@ -69,16 +83,16 @@ class Public::CounselingRoomsController < ApplicationController
   #     redirect_to category_counseling_rooms_path(params[:category_id]), alert: I18n.t('counseling_rooms.validates.user')
   #   end
   # end
-  
+
   def set_category
     @category = Category.find(params[:category_id])
   end
-  
+
   def ensure_room_creator
     counseling_room = CounselingRoom.find(params[:id])
     unless current_user == counseling_room.user
       redirect_to category_counseling_room_path(@category.id, counseling_room.id), notice: I18n.t('counseling_rooms.ensure_creator')
     end
   end
-  
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
 
   has_many :counseling_rooms, dependent: :destroy
+  
+  scope :latest, -> { order(created_at: :desc) }
+  scope :old,    -> { order(created_at: :asc) }
 
   validates :name, presence: true, uniqueness: true
 
