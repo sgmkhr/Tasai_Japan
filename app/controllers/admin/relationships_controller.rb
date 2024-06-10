8bade746ea6d727833c599b8046f6da44db29a04
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-10 01:32:54 +0000

    [Fix]pagenation指定忘れ

--- app/controllers/admin/relationships_controller.rb
+++ app/controllers/admin/relationships_controller.rb
@@ -3,12 +3,12 @@ class Admin::RelationshipsController < ApplicationController
   
   def followings
     user = User.find_by(canonical_name: params[:user_canonical_name])
-    @users = user.followings
+    @users = user.followings.where(is_active: true).page(params[:page]).per(18)
   end
   
   def followers
     user = User.find_by(canonical_name: params[:user_canonical_name])
-    @users = user.followers
+    @users = user.followers.where(is_active: true).page(params[:page]).per(18)
   end
   
 end
