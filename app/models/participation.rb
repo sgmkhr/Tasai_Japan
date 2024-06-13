a7abd49dcd4a06b78f80ca601799f68bf2fc69d9
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-13 23:54:19 +0000

    [Add]参加承認時の通知作成追加

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -3,22 +3,31 @@ class Participation < ApplicationRecord
 
   belongs_to :counseling_room
   belongs_to :user
-  
+
   has_one :notification, as: :notifiable, dependent: :destroy
-  
+
   # データが作成されると直後に通知データも作成される
   after_create do
     create_notification(user_id: counseling_room.user_id)
   end
-  
+
+   # データが更新されると直後に通知データも作成される(参加が承認された場合のみ)
+  after_update do
+    create_notification(user_id: user_id)
+  end
+
   # 表示する通知メッセージを取得するメソッド
   def notification_message
-    '「' + counseling_room.topic + '」' + I18n.t('notifications.messages.participation')
+    if status == false
+      '「' + counseling_room.topic + '」' + I18n.t('notifications.messages.participation.for_creator')
+    else
+      '「' + counseling_room.topic + '」' + I18n.t('notifications.messages.participation.for_paticipation')
+    end
   end
-  
+
   # 通知クリック時のパス先指定のメソッド
   def notification_path
-    edit_category_counseling_room_path(counseling_room.category.id, counseling_room.id)
+    category_counseling_room_path(counseling_room.category.id, counseling_room.id)
   end
 
 end
