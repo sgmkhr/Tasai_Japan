8137af2ef3df8e6f0eafd823181d3e75aea1cfe0
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-14 13:43:08 +0000

    [Update]文字数制限変更

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -8,7 +8,7 @@ class Opinion < ApplicationRecord
   
   has_many :notifications, as: :notifiable, dependent: :destroy
 
-  validates :content, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 1000 }
   
   # データが作成されると直後に通知データも作成される
   after_create do
