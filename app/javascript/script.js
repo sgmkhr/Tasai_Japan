aaf464a30acd3681691b22ff4fe6965b7f228845
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-14 13:03:44 +0000

    [Fix]JavaScript毎回発火するように修正

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -20,3 +20,24 @@ document.addEventListener("turbolinks:load", function() {
   });
   
 });
+
+window.onload = function() {
+  // ページトップへスクロールするボタンを押した時
+  $('.back_to_pagetop').on('click', function(event) {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 800);
+    event.preventDefault();
+  });
+  
+  // ヘッダーの通知アイコンを押した時
+  $('#notification-icon').on('click', function(event) {
+    $('#notifications-box').show();
+    event.preventDefault();
+  });
+  $('#close-notification-box').on('click', function(event) {
+    $('#notifications-box').hide();
+    event.preventDefault();
+  });
+  
+};
