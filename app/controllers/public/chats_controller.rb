085f24ead06554ff2d759d952c51c33aebd599ea
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-15 10:23:19 +0000

    [Update/Fix]通知一覧チャット履歴ない際の対処

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -3,12 +3,12 @@ class Public::ChatsController < ApplicationController
   before_action :ensure_guest_user
   before_action :ensure_permitted_user, only: [:index]
   before_action :reject_non_related,    only: [:show]
-  
+
   def index
     @entries = current_user.entries
     if @entries
-      @entries = Entry.where(user_id: current_user.id).sort {|a,b| 
-        b.get_last_chat.created_at <=> a.get_last_chat.created_at
+      @entries = Entry.where(user_id: current_user.id).sort {|a,b|
+        b.get_last_chat(current_user).created_at <=> a.get_last_chat(current_user).created_at
       }
     end
   end
@@ -28,45 +28,48 @@ class Public::ChatsController < ApplicationController
     @chats = @room.chats
     @chat = Chat.new
     @room.read_chats(@user) #ルーム内の相手の未読チャットを既読にするメソッド
+    if @chats && @chats.where(content: I18n.t('chats.index.zero_chat_history'))
+      @chats.where(content: I18n.t('chats.index.zero_chat_history')).destroy_all
+    end
   end
-  
+
   def create
     @chat = current_user.chats.new(chat_params)
     render :validator unless @chat.save
     # 非同期通信のため、chats/create.js.erbを呼び出す
   end
-  
+
   def destroy
     Chat.find(params[:id]).destroy
     @room = ChatRoom.find(params[:chat_room_id])
     @chats = @room.chats
     # 非同期通信のため、chats/destroy.js.erbを呼び出す
   end
-  
+
   private
-  
+
   def chat_params
     params.require(:chat).permit(:content, :chat_room_id)
   end
-  
+
   def ensure_guest_user
     if current_user.guest_user?
       redirect_to request.referer, alert: I18n.t('guestuser.validates')
     end
   end
-  
+
   def ensure_permitted_user
     user = User.find_by(canonical_name: params[:user_canonical_name])
     unless user.id == current_user.id
       redirect_to request.referer, alert: I18n.t('chats.validates.non_permitted')
     end
   end
-  
+
   def reject_non_related
     user = User.find(params[:id])
     unless current_user.following?(user) && user.following?(current_user)
       redirect_to request.referer, alert: I18n.t('chats.validates.non_related')
     end
   end
-  
+
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,15 +1,15 @@
 class Entry < ApplicationRecord
-  
+
   belongs_to :user
   belongs_to :chat_room
-  
+
   # 対象entryに紐ずくチャットルームに存在する相手userを取得するメソッド
   def get_partner(user)#引数は自分user
     chat_room.entries.each do |entry|
       return entry.user if entry.user_id != user.id
     end
   end
-  
+
   # 対象entryに紐ずくチャットルーム内に未読チャットがあるか確認するメソッド
   def is_there_unread?(user)#引数は相手user
     room = self.chat_room
@@ -20,13 +20,21 @@ class Entry < ApplicationRecord
       false
     end
   end
-  
+
   # 対象entryに紐ずくチャットルーム内の最終チャットデータを取得するメソッド
-  def get_last_chat
+  def get_last_chat(user)
     if self.chat_room.chats.exists?
       chats = Chat.latest.where(chat_room_id: self.chat_room_id)
       return chats[0]
+    else
+      #このデータがないと、チャット一覧にルーム二つ以上且つチャット履歴のないルームがある際にエラーになる
+      Chat.find_or_create_by!(user_id: user.id) do |chat|
+        chat.chat_room_id = self.chat_room_id
+        chat.content = I18n.t('chats.index.zero_chat_history')
+        chat.created_at = self.chat_room.created_at
+        chat.updated_at = self.chat_room.created_at
+      end
     end
   end
-  
+
 end
