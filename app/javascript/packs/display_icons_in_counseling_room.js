37499ae5f35522ebf04175fe3b3c64b60ce1d47a
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-06-24 06:11:44 +0000

    [Update]相談室のアイコン一覧

---
+++ app/javascript/packs/display_icons_in_counseling_room.js
@@ -0,0 +1,21 @@
+/*global $*/
+document.addEventListener("turbolinks:load", function() {
+  
+  $('#down-arrow').show();
+  $('#up-arrow').hide();
+  
+  $('#down-arrow').on('click', function() {
+    $('#icons').removeClass('limitedly-displayed-icons');
+    $(this).hide();
+    $('#up-arrow').show();
+    event.preventDefault();
+  });
+  
+  $('#up-arrow').on('click', function() {
+    $('#icons').addClass('limitedly-displayed-icons');
+    $(this).hide();
+    $('#down-arrow').show();
+    event.preventDefault();
+  });
+  
+});
